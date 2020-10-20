8b2f00b05017b5f154aded046a739b6a309b9f85
actuallyDan <dankral01@gmail.com>
2020-10-20 17:43:08 -0400

    removed extra css import

--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
 import "tachyons";
+import "./index.css";
 
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
